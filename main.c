6a9c7d9bd617b31eae99a1be194860318219b5e2
julienpierini <pierini.julien@gmail.com>
2021-02-14 00:05:25 +0100

    feat: add font + frames maths + player left and right animation

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-#include "gb/gb.h"
-#include "bcd.h"
+#include <gb/gb.h>
+#include <bcd.h>
+#include <gb/font.h>
+#include <stdio.h>
 #include "assets/map.c"
 #include "assets/tiles.c"
 #include "assets/player_tiles.c"
@@ -28,25 +30,30 @@
 /* scrolling */
 #define PXL_SC          1U              /* nb of pixels to scroll when player is moving */
 
-/* Gameboy */
-#define CPU_FREQ        4190000U        /* CPU frequency in Hertz */
-
 extern clock_t clock();
 
+font_t min_font;
 
 UBYTE player_direct;
 UBYTE tile_size, tile_flip;
 UBYTE bkg_x_pos, bkg_y_pos;
 UBYTE scrolling_speed;
-UBYTE key;
-BCD   bcd;
+UBYTE key, last_key = 0;
+UINT16 s;
+int n = 0; int frame = 0;
+int anime[3] = {7, 6, 1};
 
-UINT32 get_ms(){
-    return clock()/CPU_FREQ*1000;
+
+UINT16 get_s(){
+    return (clock()/60);
+}
+
+void print_time(){
+    printf("%d", s);
 }
 
 void init_background(){
-    set_bkg_data(0, TL_BKG_NB, background);
+    set_bkg_data(37, TL_BKG_NB, background);
     set_bkg_tiles(0, 0, mapWidth, mapHeight, map);
     scrolling_speed = PXL_SC;
 }
@@ -59,6 +66,29 @@ void init_player(){
     move_sprite( TL_PL_LEGS_NB, PL_X, PL_Y+TL_SIZE );
 }
 
+void reset_player_pos(UBYTE last_key){
+    // left  = 1
+    // rigth = 2
+    // up    = 4
+    // down  = 8
+    if(last_key == 1){
+        player_direct=0;
+    }
+    else if(last_key == 2){
+        player_direct=0;
+    }
+    else if(last_key == 4){
+        player_direct=2;
+    }
+    else if(last_key == 4){
+        player_direct=2;
+    }
+    else if(last_key == 8){
+        player_direct=1;
+    }
+    set_sprite_tile(TL_PL_LEGS_NB, player_direct*2+1);
+}
+
 void update_player_sprites(UBYTE key, UBYTE player_direct, UBYTE tile_flip){
     // set head and legs sprite depending of the player direction
     set_sprite_tile(TL_PL_HEAD_NB, player_direct*2);
@@ -69,14 +99,16 @@ void update_player_sprites(UBYTE key, UBYTE player_direct, UBYTE tile_flip){
         set_sprite_prop(TL_PL_HEAD_NB, tile_flip);
         set_sprite_prop(TL_PL_LEGS_NB, tile_flip);
         // legs anim lef/right
-        set_sprite_tile( TL_PL_LEGS_NB, 7 );
-        move_sprite(TL_PL_LEGS_NB, PL_X, PL_Y+TL_SIZE);
-
-        set_sprite_tile(TL_PL_LEGS_NB, 6);
-        move_sprite(TL_PL_LEGS_NB, PL_X, PL_Y+TL_SIZE);
-
-        set_sprite_tile( TL_PL_LEGS_NB, TL_PL_LEGS_NB);
-        move_sprite(TL_PL_LEGS_NB, PL_X, PL_Y+TL_SIZE);
+        if((frame%7) == 0){
+            set_sprite_tile(TL_PL_LEGS_NB, anime[n]);
+            n+=1;
+            if(n==3){
+                n=0;
+            }
+        }
+        // set_sprite_tile( TL_PL_LEGS_NB, 7 );
+        // set_sprite_tile(TL_PL_LEGS_NB, 6);
+        // set_sprite_tile( TL_PL_LEGS_NB, TL_PL_LEGS_NB);
 
     }
     else {
@@ -85,7 +117,6 @@ void update_player_sprites(UBYTE key, UBYTE player_direct, UBYTE tile_flip){
         set_sprite_prop(TL_PL_LEGS_NB, tile_flip);
         // set up and down sprites
         set_sprite_tile(TL_PL_LEGS_NB, player_direct*2+1);
-        move_sprite(TL_PL_LEGS_NB, PL_X, PL_Y+TL_SIZE);
     }
 }
 
@@ -138,15 +169,30 @@ void updateSwitches() {
 void main(){
     disable_interrupts();
     DISPLAY_OFF;
-    init_background();
+    
+    font_init();
+    min_font = font_load(font_min); // 36 tiles
+    font_set(min_font);
+
     init_player();
+    init_background();
+    
     DISPLAY_ON;
     enable_interrupts();
 
     while (1){
         key = joypad();
-        // int ms = get_ms();
-        // bcd = MAKE_BCD(ms);
+        if(key == 0 && last_key != 0){
+            reset_player_pos(last_key);
+        }
+        last_key = key;
+        s = get_s();
+        if((s%1) == 0){
+            frame+=1;
+            if(frame == 32){
+                frame=0;
+            }
+        }
         update_display(key);
         updateSwitches();
         wait_vbl_done();
